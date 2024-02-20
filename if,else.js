944ea82c972589014b45378f14f70b0c2fb3efcf
saimomaaktersakila <saimomaakter@gmail.com>
2024-02-20 14:30:33 +0600

    first commit

---
+++ if,else.js
@@ -0,0 +1,15 @@
+var goingtoManikganj = true;
+var MyBudget = 100;
+
+
+
+if (goingtoManikganj == true ||  MyBudget > 500 ){
+    console.log('cholo jai!!');
+}
+
+else{
+    console.log('tahole jaoya holo na!!');   
+
+}
+
+
